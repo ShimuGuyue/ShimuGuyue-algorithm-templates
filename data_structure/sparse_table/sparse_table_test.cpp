38101398127a972801f13cbf1dc55d3a6544b07c
ShimuGuyue <shimuguyue513@qq.com>
2025-12-19 20:52:02 +0800

    Update 改写ST表测试器

--- data_structure/sparse_table/sparse_table_test.cpp
+++ data_structure/sparse_table/sparse_table_test.cpp
@@ -7,6 +7,8 @@ std::mt19937 rand32(time(nullptr));
 
 
 
+int query(const std::vector<int> &v, int l, int r);
+
 void check()
 {
     int n{ static_cast<int>(rand32() % 100 + 1) };
@@ -20,36 +22,32 @@ void check()
     SparseTable st;
     st.build(as);
 
-    for (int i = 0; i < n; ++i)
+    int m{ static_cast<int>(rand32() % 10000 + 1) };
+    while (m--)
     {
-        for (int j = i; j < n; ++j)
+        int l{ static_cast<int>(rand32() % n) };
+        int r{ static_cast<int>(rand32() % n) };
+        if (l > r)
+            std::swap(l, r);
+
+        int ans_std{ query(as, l, r) };
+        int ans_st{ st.query(l, r) };
+        if (ans_std != ans_st)
         {
-            int max{ INT_MIN };
-            int index;
-            for (int k = i; k <= j; ++k)
+            std::cout << "Error!\n\n";
+
+            std::cout << n << '\n';
+            for (int i = 0; i < n; ++i)
             {
-                if (as[k] > max)
-                {
-                    max = as[k];
-                    index = k;
-                }
+                std::cout << as[i] << " \n"[i == n - 1];
             }
-            if (index != st.query(i, j))
-            {
-                std::cout << "Error!\n";
-                std::cout << n << '\n';
-                for (int i = 0; i < n; ++i)
-                {
-                    std::cout << as[i] << " \n"[i == n - 1];
-                }
-                std::cout << "error range: [" << i << "," << j << "]\n";
-                std::cout << "std ans: " << index << '\n';
-                std::cout << "ST ans: " << st.query(i, j) << '\n';
-                exit(0);
-            }
+
+            std::cout << "error range: [" << l << "," << r << "]\n";
+            std::cout << "std ans: " << ans_std << '\n';
+            std::cout << "ST ans: " << ans_st << '\n';
+            exit(0);
         }
     }
-    std::cout << "No problem!\n";
 }
 
 int main()
@@ -62,4 +60,20 @@ int main()
     {
         check();
     }
+    std::cout << "No problem!\n";
+}
+
+int query(const std::vector<int> &v, int l, int r)
+{
+    int index;
+    int max{ INT_MIN };
+    for (int i = l; i <= r; ++i)
+    {
+        if (v[i] > max)
+        {
+            max = v[i];
+            index = i;
+        }
+    }
+    return index;
 }
